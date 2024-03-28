5f5df6e7286cf9d9ca5436922a8c9f93e69ba7ed
Lindokuhle <thereconciliatedson@gmail.com>
2024-03-28 09:35:04 +0200

    creating the project using spring initializer

---
+++ src/main/java/com/drivingSchoolApp/DrivingSchoolApp/DrivingSchoolAppApplication.java
@@ -0,0 +1,13 @@
+package com.drivingSchoolApp.DrivingSchoolApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrivingSchoolAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrivingSchoolAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/drivingSchoolApp/DrivingSchoolApp/DrivingSchoolAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drivingSchoolApp.DrivingSchoolApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrivingSchoolAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
